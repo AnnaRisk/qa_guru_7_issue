dfcef218dfcb15a8d6ca53dc2dbcd2f2ca274ad0
24-002580\user <annarainchik@gmail.com>
2022-05-03 21:18:49 +0300

    DragAndDrop

--- src/test/java/qa_guru_allure/DragAndDrop.java
+++ src/test/java/qa_guru_allure/DragAndDrop.java
@@ -1,8 +1,5 @@
 package qa_guru_allure;
-
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -10,17 +7,11 @@ public class DragAndDrop {
     @Test
     public void testDragDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        SelenideElement blockA = $("#column-a");
-        SelenideElement blockB = $("#column-b");
-       //actions().dragAndDrop(blockA,blockB).release().build().perform();
-     //   actions().moveToElement(blockA).clickAndHold().moveToElement(blockB).release().build().perform();
-        actions().clickAndHold(blockA).moveToElement(blockB).release().build().perform();
+        $("#column-a").dragAndDropTo("#column-b");
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
 
     }
-
-
 }
 
 
